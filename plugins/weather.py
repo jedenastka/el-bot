59e683e3c0d3266814304f00cb6e31dc5605daac
Grzesiek11 <grzesiek.jedenastka@outlook.com>
2020-07-02 23:29:50 +0200

    Add weather command, fix function

--- plugins/weather.py
+++ plugins/weather.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from datetime import datetime
+import requests
 
 class WeatherPlugin(commands.Cog):
     def __init__(self, bot):
@@ -8,7 +9,11 @@ class WeatherPlugin(commands.Cog):
 
     @commands.command(name = 'thunder')
     async def _thunder(self, ctx):
-        await ctx.send(f"http://images.blitzortung.org/Images/image_b_pl.png?mapId={int(datetime.now().timestamp * 1000)}")
+        await ctx.send(f"http://images.blitzortung.org/Images/image_b_pl.png?mapId={int(datetime.now().timestamp() * 1000)}")
+
+    @commands.command(name = 'weather')
+    async def _weather(self, ctx, city: str):
+        await ctx.send('```' + requests.get(f'http://wttr.in/{city}?0&T&q').text + '```')
     
 def setup(bot):
     bot.add_cog(WeatherPlugin(bot))
