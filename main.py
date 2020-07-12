2ae3b97da9dca103dfcef6484abf6f85a8f111f0
Grzesiek11 <grzesiek.jedenastka@outlook.com>
2020-07-12 21:39:23 +0200

    Get prefix from database

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from discord.ext import commands
 import json
+from pymongo import MongoClient
 
 with open('secrets.json') as f_secrets:
     secrets = json.load(f_secrets)
 
-client = commands.Bot(command_prefix='eb!')
+def prefix(bot, message):
+    return bot.db['servers'].find_one({'_id': 'default'})['prefixes']
 
-client.load_extension('plugins.utils')
+bot = commands.Bot(command_prefix=prefix)
 
-client.run(secrets['token'])
+mongo = MongoClient(secrets['mongoURI'])
+bot.db = mongo['el']
+
+bot.load_extension('plugins.utils')
+
+bot.run(secrets['token'])
