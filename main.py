ba4a6fc31eb440b1f9e152d7661edffaac836ac0
Grzesiek11 <grzesiek.jedenastka@outlook.com>
2020-07-13 15:10:15 +0200

    Move prefix check to another function

--- main.py
+++ main.py
@@ -16,8 +16,6 @@ bot = discord.Client()
 mongo = MongoClient(secrets['mongoURI'])
 bot.db = mongo[settings['database']]
 
-prefix = bot.db['servers'].find_one({'_id': 'default'})['prefixes'][0]
-
 events = []
 plugins = settings['initPlugins']
 
@@ -25,12 +23,19 @@ for pluginName in plugins:
     plugin = importlib.import_module(f"{settings['pluginDir']}.{pluginName}")
     events += plugin.events
 
+def getCommand(message, event):
+    prefixes = bot.db['servers'].find_one({'_id': 'default'})['prefixes']
+    for prefix in prefixes:
+        if message.content.startswith(prefix):
+            return message.content[len(prefix):]
+    return None
+
 @bot.event
 async def on_message(message):
     for event in events:
         if event['type'] == 'command':
-            if message.content.startswith(prefix): # Add some complex prefix getting based on various things here
-                if message.content[len(prefix):] in event['alias'] + [event['name']]:
+            if getCommand(message, event) is not None:
+                if getCommand(message, event) in event['alias'] + [event['name']]:
                     await event['callable'](bot, message)
         elif event['type'] == 'onMessage':
             await event['callable'](bot, message)
