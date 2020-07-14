2fddc355b042f63fcdad490970e92bb32bb863d6
Grzesiek11 <grzesiek.jedenastka@outlook.com>
2020-07-14 08:24:03 +0200

    Now arguments preserve quotes (still kind of broken)

--- main.py
+++ main.py
@@ -5,16 +5,26 @@ from pymongo import MongoClient
 import json
 import importlib
 
+from models import Context
+
+# Load configuration files
+
 with open('settings.json') as f_settings:
     settings = json.load(f_settings)
 
 with open(settings['secretsFile']) as f_secrets:
     secrets = json.load(f_secrets)
 
+# Initialize Discord client
+
 bot = discord.Client()
+
+# Initialize database
 
 mongo = MongoClient(secrets['mongoURI'])
 bot.db = mongo[settings['database']]
+
+# Load plugins
 
 events = []
 plugins = settings['initPlugins']
@@ -23,28 +33,71 @@ for pluginName in plugins:
     plugin = importlib.import_module(f"{settings['pluginDir']}.{pluginName}")
     events += plugin.events
 
+# Helper functions
+
+def getPrefix():
+    pass
+
+def splitNoBreak(string: str):
+    splittedString = []
+    tmp = ''
+    inQuotes = False
+
+    for i in range(len(string)):
+        ch = string[i]
+        previousCh = string[i - 1] if i != 0 else ''
+        nextCh = string[i + 1] if i != len(string) - 1 else ''
+        chIsQuote = False
+        
+        if ch in ('"', '\''):
+            if not inQuotes and previousCh == ' ':
+                inQuotes = True
+                chIsQuote = True
+            elif inQuotes and nextCh in (' ', ''):
+                inQuotes = False
+                chIsQuote = True
+        
+        if (ch != ' ' or inQuotes) and not chIsQuote:
+            tmp += ch
+        
+        if nextCh == '' or (ch == ' ' and not inQuotes):
+            splittedString.append(tmp)
+            tmp = ''
+    
+    return splittedString
+
 def getCommand(message, event):
     prefixes = bot.db['servers'].find_one({'_id': 'default'})['prefixes']
     for prefix in prefixes:
         if message.content.startswith(prefix):
             commandString = message.content[len(prefix):]
-            splittedCommand = commandString.split(' ')
+
+            splittedCommand = splitNoBreak(commandString)
+
             command = {
                 'command': splittedCommand[0],
                 'args': splittedCommand[1:]
             }
+
             return command
+
     return None
+
+# Listen on Discord events and pass them to registered internal events
 
 @bot.event
 async def on_message(message):
     for event in events:
+
         if event['type'] == 'command':
             command = getCommand(message, event)
             if command is not None:
                 if command['command'] in event['alias'] + [event['name']]:
                     await event['callable'](bot, message, *command['args'])
+        
         elif event['type'] == 'onMessage':
             await event['callable'](bot, message)
+
+# Run the bot
 
 bot.run(secrets['token'])
