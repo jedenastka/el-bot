f9e4ea245b5075539f53d5f0c971a741339b5150
Grzesiek11 <grzesiek.jedenastka@outlook.com>
2020-07-26 20:37:40 +0200

    Add exception handling

--- plugins/codestats.py
+++ plugins/codestats.py
@@ -5,8 +5,12 @@ import discord
 import csplot
 
 async def c_codestats(ctx, user: str, lastDays = 7):
-    csplot.xpPlot(user, datetime.datetime.today() - datetime.timedelta(days=int(lastDays) - 1), f"tmp/codestats_{user}.png")
-    await ctx.message.channel.send(file=discord.File(fp=open(f"tmp/codestats_{user}.png", 'rb')))
+    try:
+        csplot.xpPlot(user, datetime.datetime.today() - datetime.timedelta(days=int(lastDays) - 1), f"tmp/codestats_{user}.png")
+    except Exception:
+        await ctx.message.channel.send(f"There is no user {user}...")
+    finally:
+        await ctx.message.channel.send(file=discord.File(fp=open(f"tmp/codestats_{user}.png", 'rb')))
 
 events = [
     {
