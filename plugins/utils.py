bf6855da15e68a69dd9902b0594f14dc6658adfe
Grzesiek11 <grzesiek.jedenastka@outlook.com>
2020-07-31 14:03:16 +0200

    Deleted testing commands

--- plugins/utils.py
+++ plugins/utils.py
@@ -1,31 +1,11 @@
 async def c_ping(ctx, text = 'Pong!'):
     await ctx.message.channel.send(text)
 
-async def foo(ctx, arg = ""):
-    await ctx.message.channel.send(f"foo {arg}")
-
-async def bar(ctx, arg = ""):
-    await ctx.message.channel.send(f"bar {arg}")
-
 events = [
     {
         'type': 'command',
         'name': 'ping',
         'aliases': ['test'],
         'callable': c_ping
-    },
-    {
-        'type': 'command',
-        'name': 'foo',
-        'aliases': [],
-        'callable': foo,
-        'subcommands': [
-            {
-                'type': 'command',
-                'name': 'bar',
-                'aliases': [],
-                'callable': bar
-            }
-        ]
     }
 ]
