9975d043505253fcdd7fd153231a8d1d56253320
Grzesiek11 <grzesiek.jedenastka@outlook.com>
2020-07-31 15:44:29 +0200

    Add random plugin

---
+++ plugins/random.py
@@ -0,0 +1,33 @@
+import random
+
+async def c_random(ctx, start, stop):
+    await ctx.message.channel.send(random.randrange(int(start), int(stop)))
+
+async def c_random_coin(ctx):
+    await ctx.message.channel.send(random.choice(('Heads!', 'Tails!')))
+
+async def c_random_choice(ctx, *options):
+    await ctx.message.channel.send(random.choice(options))
+
+events = [
+    {
+        'type': 'command',
+        'name': 'random',
+        'aliases': ['rand'],
+        'callable': c_random,
+        'subcommands': [
+            {
+                'type': 'command',
+                'name': 'coin',
+                'aliases': ['flip'],
+                'callable': c_random_coin
+            },
+            {
+                'type': 'command',
+                'name': 'choose',
+                'aliases': ['choice'],
+                'callable': c_random_choice
+            }
+        ]
+    }
+]
