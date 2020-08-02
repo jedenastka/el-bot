18638d53a65baabf2b172f46a476e4f16228c848
Grzesiek11 <grzesiek.jedenastka@outlook.com>
2020-08-02 16:25:36 +0200

    Add some more basic logging

--- plugins/logger.py
+++ plugins/logger.py
@@ -1,9 +1,30 @@
-async def log(ctx):
-    print(ctx.message.content)
+import datetime
+
+def log(text):
+    print(text)
+    with open('el.log', 'a') as logfile:
+        logfile.write(f"{text}\n")
+
+async def logMessages(ctx):
+    log(f"[MESSAGE] [{datetime.datetime.now().strftime(r'%d.%m.%Y %H:%M:%S')}] [{ctx.message.channel.id} {ctx.message.author.id}] {ctx.message.content}")
+
+async def logDeletes(ctx):
+    log(f"[DELETION] [{datetime.datetime.now().strftime(r'%d.%m.%Y %H:%M:%S')}] [{ctx.message.channel.id} {ctx.message.author.id}] {ctx.message.content}")
+
+async def logEdits(ctx, before):
+    log(f"[EDIT] [{datetime.datetime.now().strftime(r'%d.%m.%Y %H:%M:%S')}] [{ctx.message.channel.id} {ctx.message.author.id}] {before.content} [CHANGED TO] {ctx.message.content}")
 
 events = [
     {
         'type': 'onMessage',
-        'callable': log
+        'callable': logMessages
+    },
+    {
+        'type': 'onDelete',
+        'callable': logDeletes
+    },
+    {
+        'type': 'onEdit',
+        'callable': logEdits
     }
 ]
