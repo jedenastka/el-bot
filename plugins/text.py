d0051780ff7dda1cc524ffbb62d491c2e0572ba6
Grzesiek11 <grzesiek.jedenastka@outlook.com>
2020-08-02 17:59:27 +0200

    Fix text module

--- plugins/text.py
+++ plugins/text.py
@@ -1,43 +1,52 @@
-async def c_say(ctx, *, text):
+async def c_say(ctx, *text):
+    text = ' '.join(text)
     await ctx.message.channel.send(text)
 
-async def c_emotize(ctx, *, text):
+async def c_emotize(ctx, *text):
+    text = ' '.join(text)
     numbersInWords = {
-        0 : "zero",
-        1 : "one",
-        2 : "two",
-        3 : "three",
-        4 : "four",
-        5 : "five",
-        6 : "six",
-        7 : "seven",
-        8 : "eight",
-        9 : "nine"
+        0: 'zero',
+        1: 'one',
+        2: 'two',
+        3: 'three',
+        4: 'four',
+        5: 'five',
+        6: 'six',
+        7: 'seven',
+        8: 'eight',
+        9: 'nine'
     }
     special = {
-        ' ' : '        ',
-        '!' : ':grey_exclamation:',
-        '?' : ':grey_question:',
-        '$' : ':heavy_dollar_sign:',
-        '>' : ':arrow_forward:',
-        '<' : ':arrow_backward:',
-        '#' : ':hash:',
-        '*' : ':asterisk:',
-        '^' : ':arrow_up_small:'
+        ' ': '        ',
+        '!': ':grey_exclamation:',
+        '?': ':grey_question:',
+        '$': ':heavy_dollar_sign:',
+        '>': ':arrow_forward:',
+        '<': ':arrow_backward:',
+        '#': ':hash:',
+        '*': ':asterisk:',
+        '^': ':arrow_up_small:',
+        '@': ':fish_cake:',
+        '(': ':arrow_right_hook:',
+        ')': ':leftwards_arrow_with_hook:',
+        '-': ':heavy_minus_sign:',
+        '+': ':heavy_plus_sign:',
+        '.': ':white_circle:'
     }
 
     emotizedText = ''
     for ch in text:
         if ch.isalpha():
-            emotizedText += f":regional_indicator_{ch}:"
-        elif ch.digit():
-            emotizedText += f":{int(ch)}:"
+            emotizedText += f":regional_indicator_{ch.lower()}:"
+        elif ch.isdigit():
+            emotizedText += f":{numbersInWords[int(ch)]}:"
         elif ch in special.keys():
             emotizedText += special[ch]
 
     await ctx.message.channel.send(emotizedText)
 
-async def c_space(ctx, *, text):
+async def c_space(ctx, *text):
+    text = ' '.join(text)
     spacedText = ''
     for ch in text:
         spacedText += ch + " "
