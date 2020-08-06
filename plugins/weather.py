b1081ea5dfee7da422b3c63ddfdce17ff557a516
Grzesiek11 <grzesiek.jedenastka@outlook.com>
2020-08-06 13:15:10 +0200

    Weather plugin is more-less done

--- plugins/weather.py
+++ plugins/weather.py
@@ -4,30 +4,72 @@ import discord
 
 async def c_weather(ctx, city):
     r = requests.get('https://api.openweathermap.org/data/2.5/weather', params={'q': city, 'appid': ctx.secrets['openweathermap'], 'units': 'metric'})
-    weather = r.json()
+    response = r.json()
 
-    if weather['cod'] == 404:
+    if response['cod'] == 404:
         await ctx.send(f"There is no city `{city}`.")
         return
-    elif weather['cod'] != 200:
-        await ctx.send(f"Unknown error (error code: `{weather['cod']}`).")
+    elif response['cod'] != 200:
+        await ctx.send(f"Unknown error (error code: `{response['cod']}`).")
+        return
+    
+    emojiIcons = {
+        '01': ':sunny:',
+        '02': ':white_sun_cloud:',
+        '03': ':cloud:',
+        '04': ':cloud:',
+        '09': ':cloud_rain:',
+        '10': ':white_sun_rain_cloud:',
+        '11': ':thunder_cloud_rain:',
+        '13': ':snowflake:',
+        '50': ':fog:'
+    }
+    
+    city = response['name']
+    icon = response['weather'][0]['icon']
+    name = response['weather'][0]['main']
+    description = response['weather'][0]['description']
+    temperature = response['main']['temp']
+    temperatureFeel = response['main']['feels_like']
+    pressure = response['main']['pressure']
+    humidity = response['main']['humidity']
+
+    try:
+        wind = response['wind']['speed']
+    except:
+        wind = 0
+    
+    try:
+        clouds = response['clouds']['all']
+    except:
+        clouds = 0
+    
+    try:
+        rain = response['rain']['1h']
+    except:
+        rain = 0
+    
+    try:
+        snow = response['snow']['1h']
+    except:
+        snow = 0
     
     embed = discord.Embed(
-        title=f"Weather for {weather['name']}",
-        description=f"""**{weather['weather'][0]['main']}**
-        *{weather['weather'][0]['description']}*
+        title=f"Weather for {city}",
+        description=f"""{emojiIcons[icon[:-1]]} **{name}**
+        *{description}*
         
-        **Temperature:** {weather['main']['temp']}\u00b0C
-        *Feels like {weather['main']['feels_like']}\u00b0C*
-        **Pressure:** {weather['main']['pressure']} hPa
-        **Humidity:** {weather['main']['humidity']}%
-        **Wind speed:** {weather['wind']['speed']} m/s
-        **Cloudiness:** {weather['clouds']['all']}%
-        **Rain:** {weather['rain']['1h']} mm (last 1h)
-        **Snow:** {weather['snow']['1h']} mm (last 1h)"""
+        **Temperature:** {temperature}\u00b0C
+        *Feels like {temperatureFeel}\u00b0C*
+        **Pressure:** {pressure} hPa
+        **Humidity:** {humidity}%
+        **Wind speed:** {wind} m/s
+        **Cloudiness:** {clouds}%
+        **Rain:** {rain} mm (last 1h)
+        **Snow:** {snow} mm (last 1h)"""
     )
-    embed.set_thumbnail(url = f"https://openweathermap.org/img/wn/{weather['weather'][0]['icon']}@2x.png")
-    await ctx.send(r.text, embed=embed)
+    embed.set_thumbnail(url = f"https://openweathermap.org/img/wn/{icon}@2x.png")
+    await ctx.send(embed=embed)
 
 events = [
     {
