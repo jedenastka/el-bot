50e709446a90425d07381cf7f93279473a85affb
Grzesiek11 <grzesiek.jedenastka@outlook.com>
2020-08-06 12:30:47 +0200

    Fix error when passing none as message to context constructor

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ class Context():
 
     def __init__(self, message, bot, db, settings, secrets):
         self.message = message
-        self.send = message.channel.send
+        self.send = message.channel.send if message is not None else None
 
         self.bot = bot
         self.db = db
