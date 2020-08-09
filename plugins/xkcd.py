982453a58d4a6892000e837be59b5e65a77b3b02
Grzesiek11 <grzesiek.jedenastka@outlook.com>
2020-08-09 16:13:19 +0200

    Per-user viewers

--- plugins/xkcd.py
+++ plugins/xkcd.py
@@ -41,7 +41,7 @@ async def c_xkcd(ctx, comic='random'):
     embed.set_footer(text=response['alt'])
 
     message = await ctx.send(embed=embed)
-    viewers.update({message.id: int(comicId)})
+    viewers.update({message.id: {'id': int(comicId), 'user': ctx.message.author.id}})
     await message.add_reaction('\N{LEFTWARDS ARROW WITH HOOK}')
     await message.add_reaction('\N{LEFTWARDS BLACK ARROW}')
     await message.add_reaction('\N{TWISTED RIGHTWARDS ARROWS}')
@@ -51,41 +51,45 @@ async def c_xkcd(ctx, comic='random'):
 async def viewerInteraction(ctx, reaction, user):
     if user.id == ctx.bot.user.id:
         return
-    comicId = viewers.get(reaction.message.id)
-    if comicId is None:
+    comic = viewers.get(reaction.message.id)
+    if comic is None:
         return
     
+    if user.id != comic['user']:
+        await reaction.remove(user)
+        return
+
     if reaction.emoji == '\N{LEFTWARDS ARROW WITH HOOK}':
-        comicId = 1
+        comic['id'] = 1
     elif reaction.emoji == '\N{LEFTWARDS BLACK ARROW}':
-        comicId -= 1
+        comic['id'] -= 1
     elif reaction.emoji == '\N{TWISTED RIGHTWARDS ARROWS}':
         r = requests.get('https://c.xkcd.com/random/comic/')
-        comicId = re.search('https:\/\/xkcd.com\/(\d+)\/', r.url).group(1)
+        comic['id'] = int(re.search('https:\/\/xkcd.com\/(\d+)\/', r.url).group(1))
     elif reaction.emoji == '\N{BLACK RIGHTWARDS ARROW}':
-        comicId += 1
+        comic['id'] += 1
     elif reaction.emoji == '\N{RIGHTWARDS ARROW WITH HOOK}':
         r = requests.get('https://xkcd.com/info.0.json')
-        comicId = r.json()['num']
+        comic['id'] = r.json()['num']
     else:
         await reaction.remove(user)
         return
     
-    r = requests.get(f"https://xkcd.com/{comicId}/info.0.json")
+    r = requests.get(f"https://xkcd.com/{comic['id']}/info.0.json")
     if r.status_code != 200:
         await reaction.remove(user)
         return
     response = r.json()
 
     embed = discord.Embed(
-        title=f"{comicId} - {response['safe_title']}"
+        title=f"{comic['id']} - {response['safe_title']}"
     )
     embed.set_image(url=response['img'])
-    embed.set_author(name='xkcd.com', url=f"https://xkcd.com/{comicId}/", icon_url='https://grzesiek11.stary.pc.pl/el/xkcd2.png')
+    embed.set_author(name='xkcd.com', url=f"https://xkcd.com/{comic['id']}/", icon_url='https://grzesiek11.stary.pc.pl/el/xkcd2.png')
     embed.set_footer(text=response['alt'])
 
     await reaction.message.edit(embed=embed)
-    viewers.update({reaction.message.id: int(comicId)})
+    viewers.update({reaction.message.id: comic})
     await reaction.remove(user)
 
 events = [
