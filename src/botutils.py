1dd0cc97345f5d3f82c49e848d46a787eab027dc
Grzesiek11 <grzesiek.jedenastka@outlook.com>
2020-08-10 10:06:48 +0200

    Rewrite apropo for new database schema

--- src/botutils.py
+++ src/botutils.py
@@ -8,3 +8,32 @@ def getPrefix(message, all=False):
         if message.content.startswith(prefix):
             return prefix
     return None
+
+def getOverlayDoc(path=[]):
+    doc = db['system'].find_one({'special': 'global'})
+    
+    for element in path:
+        doc = doc.get(element, {})
+        if doc == {}:
+            break
+    
+    return doc
+
+def getServerDoc(serverId: int, path=[], addOverlay=False):
+    doc = db['servers'].find_one({'id': serverId})
+    if addOverlay:
+        doc = {**doc, **getOverlayDoc()}
+    
+    for element in path:
+        doc = doc.get(element, {})
+        if doc == {}:
+            break
+    
+    return doc
+
+def updateServerDoc(serverId: int, doc: dict, path=[]):
+    path.reverse()
+    for element in path:
+        doc = {element: doc}
+
+    db['servers'].update_one({'id': serverId}, {'$set': doc})
