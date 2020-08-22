e67e0c0332eb6693779a5465f4d86f255883f077
Grzesiek11 <grzesiek.jedenastka@outlook.com>
2020-08-22 20:06:44 +0200

    Added levels and level formula

--- plugins/levels.py
+++ plugins/levels.py
@@ -1,4 +1,5 @@
 import discord
+import math
 
 from botutils import updateServerDoc, getServerDoc
 
@@ -9,6 +10,16 @@ def addXp(userId: int, serverId: int, xp: int):
 
 def getXp(userId: int, serverId: int):
     return getServerDoc(serverId, ['plugins', 'levels', 'xp', str(userId)])
+
+def xpToLevel(x):
+    base = 2
+    hardness = 100
+    return math.floor((x/hardness) ** (1/base))
+
+def levelToXp(x):
+    base = 2
+    hardness = 100
+    return (math.floor(x) ** base) * hardness
 
 async def c_levels(ctx, user=None):
     if user is None:
@@ -26,13 +37,15 @@ async def c_levels_leaderboard(ctx):
     usersXp = getServerDoc(ctx.message.guild.id, ['plugins', 'levels', 'xp'])
     usersXp = {k: v for k, v in sorted(usersXp.items(), key=lambda item: item[1], reverse=True)}
     result = ''
+    i = 1
     for userId, xp in usersXp.items():
         user = ctx.message.guild.get_member(int(userId))
-        result += f"{user.name if user is not None else userId} - {xp}\n"
+        result += f"[{i}] {user.name if user is not None else userId} - {xpToLevel(xp)} ({xp})\n"
+        i += 1
     await ctx.send(result)
 
 
-async def c_levels_reeval(ctx):
+async def c_levels_reEval(ctx):
     usersXp = {}
     statusMsg = await ctx.send('Initializing...')
     for channel in ctx.message.guild.text_channels:
@@ -40,9 +53,9 @@ async def c_levels_reeval(ctx):
         try:
             async for message in channel.history(limit=None):
                 userId = message.author.id
-                if usersXp.get(userId) is None:
-                    usersXp[userId] = 0
-                usersXp[userId] += len(message.content)
+                if usersXp.get(str(userId)) is None:
+                    usersXp[str(userId)] = 0
+                usersXp[str(userId)] += len(message.content)
         except discord.Forbidden:
             pass
     await statusMsg.edit(content='Done!')
@@ -62,7 +75,7 @@ events = [
                 'type': 'command',
                 'name': 'reeval',
                 'aliases': [],
-                'callable': c_levels_reeval
+                'callable': c_levels_reEval
             },
             {
                 'type': 'command',
