56406fb90edd3f2c259a5fbab6c80dfe79a7c698
Grzesiek11 <grzesiek.jedenastka@outlook.com>
2020-10-25 18:31:18 +0100

    Add a "ready" message when bot is initialized

--- plugins/utils.py
+++ plugins/utils.py
@@ -1,11 +1,18 @@
 async def c_ping(ctx):
     await ctx.send('Pong!')
 
+async def readyMessage(ctx):
+    print("Ready!")
+
 events = [
     {
         'type': 'command',
         'name': 'ping',
         'aliases': ['test'],
         'callable': c_ping
+    },
+    {
+        'type': 'onReady',
+        'callable': readyMessage
     }
 ]
